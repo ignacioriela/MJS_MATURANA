ab724457f133204ec047731db5cd956260563ceb
ignacioriela <ignaciotdb17@gmail.com>
2022-07-24 23:13:49 -0300

    re

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 // Mi código JavaScript:
 
 //CONVERSION DE DINERO: https://ha.edu.uy/api/rates
+
+
+//function cambiarboton(){
+    
+//    var i=document.getElementById("boton").value = "Ver menos...";
+    
+//}
+
